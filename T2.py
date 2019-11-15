969e53cc924d9e01bbcf8b4ab0f72f6139cb784b
usama801 <usoomro716@gmail.com>
2019-11-15 16:11:58 +0500

    python programming

---
+++ T2.py
@@ -0,0 +1,5 @@
+num = int(input("Enter Any Number To Check The Number iS EVEN or ODD \n"))
+if (num%2) == 0:
+    print("Entered Number is EVEN")
+else:
+     print("Entered Number is ODD")
---
+++ T4.py
@@ -0,0 +1,11 @@
+
+list =[1,1,2,3,5,8,13,21,34,55,89]
+
+sum = 0
+
+for i in list:
+    sum = sum+i
+
+print("the sum is:" ,sum)
+
+
